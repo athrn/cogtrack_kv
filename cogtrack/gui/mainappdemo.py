2a7f422473eca3527aff25545e82cb9e7b439998
athrn <athrndev@gmail.com>
2019-04-03 15:23:01 +0200

    score screen with scrollview

--- cogtrack/gui/mainappdemo.py
+++ cogtrack/gui/mainappdemo.py
@@ -1,12 +1,22 @@
 # import setprojectpath
+from mainapp import TheApp
+from test_mainapp import make_mock_controller
+from kivy.clock import Clock
 
 def run():
-    from mainapp import TheApp
-    from test_mainapp import make_mock_controller
     app = TheApp()
     controller = make_mock_controller(app)
     app.run()
 
+def run_show_score():
+    app = TheApp()
+    controller = make_mock_controller(app)
+    score={}
+    for i, key in enumerate('abcdefghijklmnopqrstuvxyz ABCDEFGHIJKLMNOPQRSTUVXYZ'):
+        score[key] = i
+    Clock.schedule_once(lambda dt : app.show_score(score), 0)
+    app.run()
+
 
 if __name__ == "__main__":
-    run()
+    run_show_score()
